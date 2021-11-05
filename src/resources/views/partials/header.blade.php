d91494c5f73acc86c55ff072c97301055cf7bd9e
Hansanghyeon <999@hyeon.pro>
2021-11-05 23:04:59 +0900

    :sparkles: container default padding

--- src/resources/views/partials/header.blade.php
+++ src/resources/views/partials/header.blade.php
@@ -25,7 +25,9 @@
           </div>
         </div>
         <div class="flex gap-x-[17px] sm:gap-x-[36px] items-center">
-          <button class="hidden sm:block py-[11px] px-[28px] btn-black">뉴스레터 구독</button>
+          <button class="hidden sm:block py-[11px] px-[28px] btn-black">
+            뉴스레터 구독
+          </button>
           <div class="hidden sm:block w-[1px] h-[60px] bg-steam"></div>
           <button 
             class="btn-search" 
