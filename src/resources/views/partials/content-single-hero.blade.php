a60c9947e414a2745624c114f6577887547e9f9c
Hansanghyeon <999@hyeon.pro>
2021-11-07 15:26:24 +0900

    :construction: hero post card markup

---
+++ src/resources/views/partials/content-single-hero.blade.php
@@ -0,0 +1,22 @@
+<article class="content__hero">
+  <div class="content__hero-thumbnail">
+    <img src="https://picsum.photos/1920?random=0">
+    <a href="#" class="content__hero-thumbnail-label btn-deepSpaceRodeo">
+      Talent Bank 스토리
+    </a>
+  </div>
+  <div class="content__hero-meta coldiv__s2">
+    <time datetime="2021-10-20">2021/10/20</time><a href="#" class="content__hero-meta-term">탤런트뱅크 소식</a>
+  </div>
+  <div class="content__hero-main">
+    <a href="#" class="content__hero-title underline power">
+      휴넷 탤런트뱅크, <br/>
+      출시 3년만 '긱 경제' 대표 플랫폼으로 성장
+    </a>
+    <div>
+      디지털 혁신 시대의 도래디지털 혁신이란 무엇일까? 많은 정의가 있지만 '기업의 디지털화를 통해
+      개선된 고객 경험을 창출하고 기업 전략 프로세스 효율성을 개선하는 과정'이라
+      디지털 혁신 시대의 도래디지털 혁신이란 무엇일까? 많은 정의가 있지만 '기업의 디지털화를...
+    </div>
+  </div>
+</article>
--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -80,5 +80,8 @@ module.exports = {
       },
     },
   },
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 };
