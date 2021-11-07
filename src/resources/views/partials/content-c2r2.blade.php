ed1af993efe441311c992dbb7678728a81696502
Hansanghyeon <999@hyeon.pro>
2021-11-07 16:16:33 +0900

    :sparkles: c2r2 content markup

---
+++ src/resources/views/partials/content-c2r2.blade.php
@@ -0,0 +1,23 @@
+<div class="content__c2r2">
+  <div>
+    <div class="content__c2r2-meta listDivider__cols2">
+      <time datetime="2021-10-20">2021/10/20</time>
+      <div class="content__c2r2-meta-term">
+        <a href="#" class="btn-seaSerpent">시리즈</a>
+        <a href="#">탤런트뱅크 소식</a>
+      </div>
+    </div>
+    <a href="#" class="content__c2r2-title">
+      '고급인력 공유' 신고용시대 연다
+    </a>
+  </div>
+  <div class="content__c2r2-thumbnail">
+    <img src="https://picsum.photos/1920?random=0">
+  </div>
+  <div class="content__c2r2-main col-span-2 prose text-[18px]">
+    <p>
+      인력 매칭 플랫폼 '탤런트뱅크'  기존 헤드헌팅 업체들과 달리 하루부터 수개월까지 단기 채용
+      중소기업 인건비 부담 덜어주고 고경력 전문가들 역량 발휘 기회기업은 사업의...
+    </p>
+  </div>
+</div>
