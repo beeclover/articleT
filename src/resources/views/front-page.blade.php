98c32c37f0d1c72b585e26aa457d502c63cc6e26
Hansanghyeon <999@hyeon.pro>
2021-11-07 17:08:47 +0900

    g:construction: component layer 컴포넌트화

--- src/resources/views/front-page.blade.php
+++ src/resources/views/front-page.blade.php
@@ -4,7 +4,7 @@
 
 <section>
   <div class="container mx-auto grid grid-cols-1 sm:grid-cols-[1fr,1px,1fr] gap-x-[52px]">
-    <div class="sm:py-[100px] mb-[52px] sm:mb-0">
+    <div class="sm:py-[100px] mb-[54px] sm:mb-0">
       @include('partials.content-hero')
       <div class="sm:h-px bg-steam w-full mt-[60px] sm:mb-[36px]"></div>
       <div class="py-[20px] sm:py-0 bg-[#f7f7f7] sm:bg-transparent px-[var(--container-px)] sm:px-0 mx-full sm:mx-0">
@@ -12,9 +12,9 @@
       </div>
     </div>
     <div class="hidden sm:block bg-steam"></div>
-    <div class="sm:py-[100px]">
-      <div class="text-[32px] font-bold mb-[72px]">인기 포스트</div>
-      <ul class="listDivider__rows1">
+    <div class="sm:py-[100px] mb-[54px] sm:mb-0">
+      <div class="text-[24px] sm:text-[32px] font-bold mb-[45px] sm:mb-[72px]">인기 포스트</div>
+      <ul class="sm:listDivider__rows1 grid gap-y-[54px] sm:gap-y-[88px]">
         <li>
           @include('partials.content-c2r2')
         </li>
