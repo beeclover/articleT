18afb1db95c77ced239a5a0eec881471dba00e0c
Hansanghyeon <999@hyeon.pro>
2021-11-07 15:27:20 +0900

    :sparkles: banner 클라이언트 이용안내

---
+++ src/resources/views/partials/banner/client_announcement.blade.php
@@ -0,0 +1,7 @@
+<div class="flex items-center justify-between gap-x-[24px]">
+  <div class="flex items-center gap-x-[18px]">
+    <img src="{!! get_theme_file_uri('resources/images/client_announcement-01.svg') !!}" alt="">
+    <span class="text-[28px] text-blackOut">업무에 도움을 줄 전문가를 찾고 계신가요?</span>
+  </div>
+  <a href="" class="underline text-[#888] hover:text-blackOut">Client 이용 안내 보기</a>
+</div>
