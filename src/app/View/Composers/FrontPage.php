a3ff193bb3d8df8017e6aaa0ae355aa853b468d1
Hansanghyeon <999@hyeon.pro>
2021-11-09 00:39:42 +0900

    :sparkles: post hook

--- src/app/View/Composers/FrontPage.php
+++ src/app/View/Composers/FrontPage.php
@@ -26,17 +26,34 @@ class FrontPage extends Composer
     {
         return [
           'heroPost' => $this->heroPost(),
+          'popularityPosts' => $this->popularityPosts(),
         ];
     }
 
     public function heroPost()
     {
-      $post = get_field('main-hero_post', 'option');
-      $post = new Hook($post[0]);
-      $post = $post->setPostData();
+      $taxonomies = ['category' => ['insight_category', 'future_lab_category', 'tb_story_category']];
+      $posts = get_field('main-hero_post', 'option');
+
+      $post = ((new Hook($posts, $taxonomies))::$posts)[0];
       unset($post->post_content);
       $post_type_obj = get_post_type_object( get_post_type($post->ID) );
       $post->postTypeLabel = $post_type_obj->labels->singular_name;
       return $post;
+    }
+
+    public function popularityPosts()
+    {
+      $taxonomies = [
+        'category' => ['insight_category', 'future_lab_category', 'tb_story_category'],
+        'collection',
+      ];
+
+      $posts = get_field('main-popularity_post', 'option');
+      $posts = (new Hook($posts, $taxonomies))::$posts;
+      return array_map(function($post) {
+        unset($post->post_content);
+        return $post;
+      }, $posts);
     }
 }
