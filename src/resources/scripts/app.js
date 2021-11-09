f7f6cb3f1cdff1ed734018559ccdf51f4c39b296
Hansanghyeon <999@hyeon.pro>
2021-11-10 00:50:12 +0900

    :construction: swiperjs init app.js

--- src/resources/scripts/app.js
+++ src/resources/scripts/app.js
@@ -1,8 +1,17 @@
 import 'jquery';
 import Alpine from 'alpinejs';
 import intersect from '@alpinejs/intersect';
+import SwiperCore, {
+  Navigation,
+  Pagination,
+  EffectFade,
+  Autoplay,
+  Controller,
+} from 'swiper/core';
+import 'swiper/css/bundle';
 
 Alpine.plugin(intersect);
+SwiperCore.use([Navigation, Pagination, EffectFade, Autoplay, Controller]);
 
 // componments
 import header from './components/header';
