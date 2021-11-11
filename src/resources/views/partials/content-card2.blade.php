050e79c4b1a419dba8c5bc76a4f62e573d8b09a3
Hansanghyeon <999@hyeon.pro>
2021-11-11 12:01:42 +0900

    :construction: single post content__card 스타일 추가

---
+++ src/resources/views/partials/content-card2.blade.php
@@ -0,0 +1,41 @@
+<div class="content__card">
+  <div class="content__card-meta listDivider__cols2">
+    <time datetime="{!! $date !!}">{!! $date !!}</time>
+    @if ($collection)
+      @foreach ($collection as $term)
+        <div class="content__card-meta-term">
+          <a href="{!! $term->link !!}" class="btn-seaSerpent">{!! $term->name !!}</a>
+          @if (count($term->children) > 0)
+              @foreach ($term->children as $child)
+                  <a href="{!! $child->link !!}">{!! $child->name !!}</a>
+              @endforeach
+          @endif
+        </div>
+      @endforeach
+    @elseif($category)
+      @foreach ($category as $term)
+        <div class="content__card-meta-term">
+          <a href="{!! $term->link !!}">{!! $term->name !!}</a>
+        </div>
+      @endforeach
+    @endif
+  </div>
+  <div class="content__card-body">
+    <div class="content__card-title">
+      <a href="{!! $permalink !!}">{!! $title !!}</a>
+    </div>
+  </div>
+  <div class="content__card-thumbnail">
+    <img src="{!! $thumbnail !!}">
+  </div>
+  <div class="content__card-main col-span-2">
+    <div class="prose">
+      <p class="break-all">
+        {!! $excerpt !!}
+      </p>
+    </div>
+    <div class="hidden mt-[16px]">
+      <a href="{!! $permalink !!}" class="underline power" style="--uph: 1px">Read on</a>
+    </div>
+  </div>
+</div>
