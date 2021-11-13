717aeef88cf87c237588aaff8757d1cca344f7e2
Hansanghyeon <999@hyeon.pro>
2021-11-13 17:31:03 +0900

    :construction: [WIP] sns share mockup

---
+++ src/resources/views/components/sns-share.blade.php
@@ -0,0 +1,11 @@
+<div class="sns flex  gap-x-[16px]">
+  <a href="#" class="sns-icon">
+    facebook
+  </a>
+  <a href="#" class="sns-icon">
+    twitter
+  </a>
+  <a href="#" class="sns-icon">
+    instagram
+  </a>
+</div>
