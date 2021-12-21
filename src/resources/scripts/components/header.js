a26de6f81bf024a5199699a98a48b7e52bdf4063
Hansanghyeon <999@hyeon.pro>
2021-12-21 15:17:15 +0900

    :construction: scroll headroom 버그 수정

--- src/resources/scripts/components/header.js
+++ src/resources/scripts/components/header.js
@@ -1,8 +1,11 @@
 import Headroom from 'headroom.js';
 
 function header() {
-  const targetEl = document.querySelector('#headroom');
-  const headroom = new Headroom(targetEl);
+  const targetEl = document.querySelector('#global');
+  const headroom = new Headroom(targetEl, {
+    tolerance: 5,
+    offset: 80,
+  });
   headroom.init();
 }
 
