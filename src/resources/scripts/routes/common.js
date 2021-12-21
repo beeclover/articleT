1aea8a2d7edfcb19d363bc00e9acfc40cb7d0b80
Hansanghyeon <999@hyeon.pro>
2021-12-21 15:16:18 +0900

    :recycle: routes

---
+++ src/resources/scripts/routes/common.js
@@ -0,0 +1,13 @@
+import header from '../components/header';
+import shrink from '../components/shrink';
+import date from '../components/date';
+
+export default {
+  containerInit() {},
+  init() {
+    header();
+    shrink();
+    date();
+  },
+  finalize() {},
+};
