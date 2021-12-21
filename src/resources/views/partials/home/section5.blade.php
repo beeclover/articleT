234f60f85980889c9ada3f8066b327a7ad9b0107
Hansanghyeon <999@hyeon.pro>
2021-12-21 11:54:31 +0900

    pptx 1-1

--- src/resources/views/partials/home/section5.blade.php
+++ src/resources/views/partials/home/section5.blade.php
@@ -3,7 +3,7 @@
 --}}
 
 <section class="overflow-hidden">
-  <div class="container mx-auto sm:pt-[60px] py-[50px] sm:pb-[150px]">
+  <div class="container mx-auto sm:py-[80px] py-[50px]">
     <h2 class="text-center text-[24px] sm:text-[34px] font-bold mb-[28px] sm:mb-[32px]">컬렉션</h2>
     <div class="mb-[70px]">
       <x-PostsCollection />
