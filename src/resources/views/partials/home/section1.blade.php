adbf2b42d11f47d9b2477f3caeff45ee87c06296
Hansanghyeon <999@hyeon.pro>
2022-01-30 19:14:59 +0900

    :sparkles: 데이터 바인딩

--- src/resources/views/partials/home/section1.blade.php
+++ src/resources/views/partials/home/section1.blade.php
@@ -4,22 +4,24 @@
 
 <section id="swiper-hero" class="overflow-hidden">
   <div class="swiper-wrapper">
-    @foreach ([1,2,3] as $item)
+    @foreach ($bannerMain as $post)
       <div class="swiper-slide">
         <div class="container mx-auto min-h-[400px] flex items-center relative z-10 py-[80px]">
           <div class="px-[50px]">
-            <h1 class="text-[color:#242424] text-[48px] leading-[68px] mb-[16px]">
-              기업 고민,<br/>
-              전문가가 직접 해결 합니다.
+            <h1 class="@if($post->isBlackText) text-[color:#242424] @else text-white @endif text-[48px] leading-[68px] mb-[16px]">
+              {!! $post->banner_title !!}
             </h1>
-            <p class="text-[20px] text-[color:#3b3b3b] mb-[33px]">
-              필요한 순간, 필요한 전문가를<br/>
-              탤런트뱅크에서 만날 수 있습니다.
+            <p class="text-[20px] @if($post->isBlackText) text-[color:#3b3b3b] @else text-white @endif mb-[33px]">
+              {!! $post->banner_content !!}
             </p>
-            <button class="btn-deepSpaceRodeo rounded-full text-[18px] font-normal px-[28px]">test</button>
+            <button class="btn-deepSpaceRodeo rounded-full text-[18px] font-normal px-[28px]" target="{!! $post->banner_button_link_target !!}">{!! $post->banner_button_text !!}</button>
           </div>
         </div>
-        <img class="absolute w-full h-full object-cover position-center" src="https://picsum.photos/1920/1080?random={!! $item !!}">
+        @if ($post->thumbnail_uri)
+          <img class="absolute w-full h-full object-cover position-center" src="{!! $post->thumbnail_uri !!}">
+        @else
+          <img class="absolute w-full h-full object-cover position-center" src="/wp-content/uploads/2022/01/4263.jpg">
+        @endif
       </div>
     @endforeach
   </div>
