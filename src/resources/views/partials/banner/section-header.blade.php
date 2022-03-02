6f92c015d16f3cba7135efbffd91eaa051dbfaf7
Hansanghyeon <999@hyeon.pro>
2022-03-02 18:42:26 +0900

    :truck: banner header > banner section-header

--- src/resources/views/partials/banner/section-header.blade.php
+++ src/resources/views/partials/banner/section-header.blade.php
@@ -1,23 +1,23 @@
-<div class="header__page">
-  <div class="header__page-inner relative">
-    <div class="header__page-labelWrap {!! $labelBg !!}">
-      <div class="header__page-label">
+<div class="header__section">
+  <div class="header__section-inner relative">
+    <div class="header__section-labelWrap {!! $labelBg !!}">
+      <div class="header__section-label">
         {!! $label !!}
       </div>
     </div>
-    <div class="header__page-main">
+    <div class="header__section-main">
       <div class="text-center">
-        <h1 class="header__page-title">{!! $title !!}</h1>
-        <div class="header__page-subtitle">
+        <h1 class="header__section-title">{!! $title !!}</h1>
+        <div class="header__section-subtitle">
           {!! $content !!}
         </div>
       </div>
     </div>
     <div class="absolute w-full h-full left-0 top-0 right-0 bottom-0">
       @if(empty($bgImg) || is_null($bgImg))
-        @include('partials.img', ['ex' => 'jpg', 'name' => 'header-03', 'class' => 'header__page-bg'])
+        @include('partials.img', ['ex' => 'jpg', 'name' => 'header-03', 'class' => 'header__section-bg'])
       @elseif(!empty($bgUrl))
-        <img src="{!! $bgUrl !!}" class="header__page-bg" alt="">
+        <img src="{!! $bgUrl !!}" class="header__section-bg" alt="">
       @else
         {!! $bgImg !!}
       @endif
